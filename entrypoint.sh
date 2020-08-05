dc0205ad1db65291569d6474e5dca7b298a3c8c5
Amndeep Singh Mann <amann@mitre.org>
2020-08-05 18:36:53 -0400

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo 'hi'
-ls
-ls /
 cd /control-table-data-ingest
 node ./ingest.js
-cp -r ./profiles /github/workspace/control_table_data_ingest/profiles
+cp -r ./profiles /github/workspace/src/assets/data/baselineProfiles/
